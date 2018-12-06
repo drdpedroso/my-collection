ecba3b54d57ead531cbf787596d14a0cdbfef89b
Eduardo <drdpedroso@gmail.com>
2018-12-06 09:05:53 +0100

    First

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+import logo from '../images/logo.png';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Image
+                    source={logo}
+                    style={{width: 80, height: 50}}
+                />
+                
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
